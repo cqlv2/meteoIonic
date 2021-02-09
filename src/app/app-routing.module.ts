4621a88f2c98c780ba0a3bc4c3789cf27dcc1079
cql <samianto@gmail.com>
2021-02-09 15:22:52 +0100

    init2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'wearher',
+    redirectTo: 'weather',
     pathMatch: 'full'
   },
   {
