84a6114c924ca2bf49232e6386df04e394cbe6a8
cql <samianto@gmail.com>
2021-02-09 16:11:38 +0100

    bien centre sur nonus meme

--- src/app/weather/map/map.component.ts
+++ src/app/weather/map/map.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {Map,tileLayer,marker} from 'leaflet';
 
 @Component({
   selector: 'app-map',
@@ -7,8 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
-  constructor() { }
+  map:Map;
+  newMarker:any;
+  address:string[];
+  
+  constructor(private router:Router) { }
+
+
 
   ngOnInit() {}
 
+ // The below function is added
+ ionViewDidEnter(){
+  this.loadMap();
+}
+// The below function is added
+loadMap(){
+  this.map = new Map("mapId").setView([46.6311634, 3.0599573], 5);
+  
+  
+  
+  tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  { attribution: 'Map data © <a  href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY- SA</a>'})
+  .addTo(this.map);
+}
+goBack(){
+  this.router.navigate(["home"]);
+
+
+}
+
+  
+  
 }
