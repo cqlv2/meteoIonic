d858caa46ef3e7e425e7b1bce5615f301729e341
cql <samianto@gmail.com>
2021-02-10 11:30:28 +0100

    mycommit

--- src/app/weather/map/map.component.ts
+++ src/app/weather/map/map.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Map, tileLayer, marker } from 'leaflet';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+
 
 @Component({
   selector: 'app-map',
@@ -13,7 +15,7 @@ export class MapComponent implements OnInit {
   newMarker: any;
   address: string[];
 
-  constructor(private router: Router) { }
+  constructor(private geoL: Geolocation, private router: Router) { }
 
 
 
@@ -23,6 +25,7 @@ export class MapComponent implements OnInit {
   ionViewDidEnter() {
     this.loadMap();
   }
+
   // The below function is added
   loadMap() {
     this.map = new Map("mapId").setView([46.6311634, 3.0599573], 5);
@@ -31,24 +34,36 @@ export class MapComponent implements OnInit {
       .addTo(this.map);
   }
 
+
+
+
+
+
   locatePosition() {
-    this.map.locate({ setView: true }).on("locationfound", (e: any) => {
-      this.newMarker = marker([e.latitude, e.longitude], {
-        draggable:
-          true
-      }).addTo(this.map);
+    this.geoL.getCurrentPosition().then(coordonee => {
+      this.newMarker = marker([coordonee.coords.latitude, coordonee.coords.longitude],
+        {
+          draggable:
+            false
+        }).addTo(this.map);
       this.newMarker.bindPopup("You are located here!").openPopup();
 
-      this.newMarker.on("dragend", () => {
-        const position = this.newMarker.getLatLng();
-      });
-    });
+      this.moveToCity(coordonee.coords.latitude, coordonee.coords.longitude)
+
+    }).catch(e => console.log(e))
   }
+
+
+
+
+
+
+
+
 
   moveToCity(lat: number, lon: number) {
     this.map.setView([lat, lon], 12)
   }
-
 
   goBack() {
     this.router.navigate(["home"]);
